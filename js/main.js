e76ffc45b2dba51f0acd6112775bd7e8a10252d0
gongshw <gongshw1992@gmail.com>
2013-12-02 23:41:50 +0800

    new post highlight-link-to-current-page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function () {
+	var str = location.href.toLowerCase();
+	$(".nav li a").each(function () {
+		if (str.indexOf(this.href.toLowerCase()) > -1) {
+			$("li.active").removeClass("active");
+			$(this).parent().addClass("active");
+		}
+	});
+})
