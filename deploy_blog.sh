528c4bcae18b916b4442557a69196421a1caaa38
Gongshw <gongshw@gongshw-mb-pro.local>
2015-12-10 22:22:06 +0800

    fix deploy bug

---
+++ deploy_blog.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ $# -eq 0 ]; then
+    echo "git need a commit message!"
+    exit 1
+fi
+cd ~/raw.gongshw.github.io; 
+jekyll build || error_exit "$LINENO: jekyll build failed";
+git add .;
+git commit -am "${1}";
+git push;
+cd ../gongshw.github.io/;
+git pull
+rm -rf ./*;
+cp -r ../raw.gongshw.github.io/_site/* .;
+git add .;
+git commit -am "${1}";
+git push;
+
